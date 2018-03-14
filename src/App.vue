582a76624b44ed92f6bfc15b77df08d22477f9e3
udcun <duphungcong@gmail.com>
2018-03-14 11:48:31 +0700

    done/progress bar

--- src/App.vue
+++ src/App.vue
@@ -183,6 +183,31 @@
     props: {
       source: String
     },
+    created() {
+      //  [App.vue specific] When App.vue is first loaded start the progress bar
+      this.$Progress.start()
+      //  hook the progress bar to start before we move router-view
+      this.$router.beforeEach((to, from, next) => {
+        //  does the page we want to go to have a meta.progress object
+        if (to.meta.progress !== undefined) {
+          let meta = to.meta.progress
+          // parse meta tags
+          this.$Progress.parseMeta(meta)
+        }
+        //  start the progress bar
+        this.$Progress.start()
+        //  continue to next page
+        next()
+      })
+      //  hook the progress bar to finish after we've finished moving router-view
+      this.$router.afterEach((to, from) => {
+        if (to.name !== 'ErrorPage') {
+          this.menuItem = to.name
+        }
+        //  finish the progress bar
+        this.$Progress.finish()
+      })
+    },
     computed: {
       user () {
         return this.$store.getters.user
@@ -205,7 +230,10 @@
         this.$store.dispatch('logOut')
         this.$router.push('/login')
       }
-    }
+    },
+    mounted() {
+    this.$Progress.finish()
+  }
   }
 </script>
 
